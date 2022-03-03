0543e4b4bb0b4dc24e9a591a7e8223fbf199717d
Kristian Aars <kristian@aars.me>
2022-03-03 20:56:05 +0100

    The API now uses sign instead of letters to describe Operator

--- src/main/java/no/ntnu/idi/idatt2105/krisvaa/Assigment4_Backend/model/CalculationRequest.java
+++ src/main/java/no/ntnu/idi/idatt2105/krisvaa/Assigment4_Backend/model/CalculationRequest.java
@@ -2,6 +2,9 @@ package no.ntnu.idi.idatt2105.krisvaa.Assigment4_Backend.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
 
 public class CalculationRequest {
 
@@ -61,17 +64,22 @@ public class CalculationRequest {
 }
 
 enum Operator {
-    Plus, Minus, Divide, Times;
+    Plus('+'), Minus('-'), Divide('/'), Times('x');
 
-    /*public static Operator parseOperator(char o) throws Exception {
-        switch (o) {
-            case '+': return Plus;
-            case '-': return Minus;
-            case '/': return Divide;
-            case '*': return Times;
-            default: {
-                throw new Exception("Unable to parse operator from " + o);
-            }
-        }
-    }*/
+    private char sign;
+
+    private Operator(char sign) {
+        this.sign = sign;
+    }
+
+    @JsonCreator
+    public static Operator decode(final char sign) {
+        return Arrays.stream(Operator.values()).filter(operator -> operator.sign == sign).findFirst().orElse(null);
+    }
+
+    @JsonValue
+    public char getSign() {
+        return sign;
+    }
+
 }
