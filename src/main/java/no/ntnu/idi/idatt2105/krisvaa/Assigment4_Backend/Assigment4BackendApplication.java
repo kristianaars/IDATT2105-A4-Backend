6f0da0edef3f614d85d9044a9abd65382871f526
Kristian Aars <kristian@aars.me>
2022-03-03 11:47:36 +0100

    Initial commit

---
+++ src/main/java/no/ntnu/idi/idatt2105/krisvaa/Assigment4_Backend/Assigment4BackendApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idi.idatt2105.krisvaa.Assigment4_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assigment4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assigment4BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/idi/idatt2105/krisvaa/Assigment4_Backend/Assigment4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idi.idatt2105.krisvaa.Assigment4_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assigment4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
